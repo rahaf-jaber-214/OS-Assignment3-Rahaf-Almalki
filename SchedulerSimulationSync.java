a82080c892a02879134c15f610534d1d1d959576
rahaf-jaber-214 <445052147@std.psau.edu.sa>
2026-04-29 15:57:14 +0300

    waiting time

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -74,8 +74,12 @@ public static final ReentrantLock contextSwitchLock = new ReentrantLock();
 
     // Method to add waiting time
     public static void addWaitingTime(long time) {
-        // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+        waitingTimeLock.lock();
+        try {
+            totalWaitingTime += time;
+        } finally {
+            waitingTimeLock.unlock();
+        }
     }
 
     // Method to log execution
